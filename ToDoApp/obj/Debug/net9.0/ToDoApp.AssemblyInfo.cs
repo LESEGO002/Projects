822e56c75157b48857a901aeb8c4d7196a6b8326
ST1047510 <ST10475105@rcconnect.edu.za>
2026-02-24 08:11:29 +0200

    updated

--- ToDoApp/obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net9.0/ToDoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8708927b52c1afc6e7998e54a237feb8f788144e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d9d9125d2147f406fb47939549b230e20d62ab8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
